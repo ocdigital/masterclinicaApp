26bda1136d5a5785d3d34b29177d8581c1bd9e16
Eduardo Ferreira <eduardo@ocdigital.com.br>
2020-09-21 22:45:23 -0300

    Subindo

---
+++ precache-manifest.9bb9e5c2650a9804d3c7ea1c6b269a22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6adf643dc1f29558c2bb25d3490d81cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e45e730b1e3ac53dd6e3",
+    "url": "/static/js/2.807cb767.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.807cb767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "635c0119b6b3c3369b63",
+    "url": "/static/js/main.eb9dd6e3.chunk.js"
+  },
+  {
+    "revision": "ea43f55c026fd7fda733",
+    "url": "/static/js/runtime-main.e8930907.js"
+  }
+]);
